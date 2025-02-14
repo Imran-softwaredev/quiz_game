0d0976caa71fd8339f1dbf3fda2bb802b3f53977
Imran Khan <imransoftwaredev09@gmail.com>
2025-02-14 10:53:47 -0500

    Minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,7 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      //   colors: {
-      //     primary: "#1D4ED8",
-      //     secondary: "#9333EA",
-      //     accent: "#FACC15",
-      //     background: "#F3F4F6",
-      //     textPrimary: "#1F2937",
-      //     textSecondary: "#6B7280",
-      //   },
-    },
+    extend: {},
   },
   plugins: [],
 };
